ae6755cb0eea06c3bc492c4b449a8fd496e8e4d7
kristinmoon <71245424+kristinmoon@users.noreply.github.com>
2021-01-17 14:27:02 -0700

    add code and files from module 14

---
+++ db/schema.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS lets_get_technical_db;
+
+CREATE DATABASE lets_get_technical_db;
---
+++ models/Comment.js
@@ -0,0 +1,40 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model { }
+
+Comment.init(
+  {
+    comment_text: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        // the comment must be at least 1 character long
+        len: [1]
+      }
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id'
+      }
+    },
+    post_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'post',
+        key: 'id'
+      }
+    }
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comment'
+  }
+);
+
+module.exports = Comment;
